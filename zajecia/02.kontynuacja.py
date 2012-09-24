437f83b1e544cb94fe14b9e344e68757c48e2e86
Krzysztof Trzewiczek <krzysztof@trzewiczek.info>
2012-09-24 22:58:31 +0200

    Komentarze do 02 skończone

--- zajecia/02.kontynuacja.py
+++ zajecia/02.kontynuacja.py
@@ -31,42 +31,63 @@ for ziomek in ziomy:
     else:
         print "Kto nie ma dzieci? " + ziomek[1]
 
+# odstęp wielokreskowy
 print '_' * 50
 
+# przygotowujemy sobie puste kubełki, żeby pogrupować naszych ziomków
 urodzaj = []
 dwoje   = []
 jedno   = []
 zero    = []
 
+# i znów przyjrzymy się im ziomek po ziomku
 for ziomek in ziomy:
+    # liczba dzieci
     dzieci = len(ziomek[3])
+    # wiek ziomka
     wiek   = ziomek[2]
 
-    if dzieci > 2 and wiek < 30:
-        urodzaj.append( ziomek )
+    # jeśli ma więcej niż dwójkę dzieli LUB ma poniżej 30 lat
+    if dzieci > 2 or wiek < 30:
+        # dodaj go do kubełka "urodzaj"
+        urodzaj.append(ziomek)
+    # w przeciwnym wypadku, jeśli (elif) ma dokładnie dwójkę dzieci
     elif dzieci is 2:
+        # dodaj go do kubełka "dwoje"
         dwoje.append(ziomek)        
+    # w przeciwnym wypadku, jeśli (elif) ma dokładnie jedno dziecko
     elif dzieci is 1:
+        # dodaj go do kubełka "dwoje"
         jedno.append(ziomek)
+    # jeśli to żaden z powyższych przypadków (czyli w ogóle nie ma dzieci)
     else:
+        # dodaj go do kubełka "zero"
         zero.append(ziomek)
 
+# ukladamy sobie kubełki w kolejności malejącej
 posortowane = [ urodzaj, dwoje, jedno, zero ]
 
-for grupa in posortowane:
+# dla każdego kubełka na tej liście
+for kubelek in posortowane:
+    # najpierw narysujemy odkresownik
     print "|" * 50
 
-    for ziomek in grupa:
+    # a później wypiszemy imię każdego ziomka w tym kubełku
+    for ziomek in kubelek:
         print ziomek
 
 
-
+# odstęp wielokreskowy
 print '_' * 50
 
+# sprawdzamy każdego ziomka na naszej liście ziomów
 for ziomek in ziomy:
+    # pytamy go o imiona dzieci
     dzieci = ziomek[3]
 
+    # bierzemy imię każdego z jego dzieci
     for dziecko in dzieci:
+        # sprawdzamy czy jego/jej imię zawiera (in) literę 'g'
         if 'g' in dziecko:
+            # jeśli tak, to wypisujemy nazwisko zioma i imię dziecka
             print ziomek[1] + ": " + dziecko
-            
