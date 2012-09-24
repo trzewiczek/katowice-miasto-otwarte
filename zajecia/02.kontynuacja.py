64f17ee163d7a526c9f2caa1b4bba828e481e680
Krzysztof Trzewiczek <krzysztof@trzewiczek.info>
2012-09-24 22:39:50 +0200

    Komentarze cz.I

--- zajecia/02.kontynuacja.py
+++ zajecia/02.kontynuacja.py
@@ -1,20 +1,35 @@
 # coding: utf-8
 
+# tworzymy pięć teczek dla pięciu osób
+# każda osoba opisana jest za pomocą takich atrybutów:
+#   - imię
+#   - nazwisko
+#   - wiek
+#   - lista imion dzieci
+#   - zawód
 osoba1 = ['Jan'      , 'Kowalski'  , 23, ['Jasio']                         , 'Programista']
 osoba2 = ['Anna'     , 'Nowak'     , 50, ['Kasia', 'Wiola']                , 'Programistka']
 osoba3 = ['Stanisław', 'Wiśniewski', 54, []                                , 'Malarz']
 osoba4 = ['Wioletta' , 'Hałas'     , 63, ['Zbigniew']                      , 'Wiolonczelistka']
 osoba5 = ['Frania'   , 'Guzik'     , 28, ['Jagna', 'Magda', "Dorota"]      , 'Architektka']
 
+# zbieramy wszystkie te osoby w jedną listę ziomów
 ziomy = [ osoba1, osoba2, osoba3, osoba4, osoba5 ]
 
+# przyjrzyjmy się każdemu ziomów
 for ziomek in ziomy:
-    if len(ziomek[3]) > 1:
-        print "Niezły urodzaj: " + str(ziomek[3])
+    # wyjmujemy sobie listę dzieci danego ziomka
+    dzieci = ziomek[3]
+
+    # jeśli dzieci więcej niż jedno, wypisz wszystkie ich imiona
+    if len(dzieci) > 1:
+        print "Niezły urodzaj: " + ", ".join(dzieci)
+    # w przeciwnym razie, jeśli (elif) ma jedno dziecko, wypisz jego imię
     elif len(ziomek[3]) is 1:
-        print "O masz dziecko: " + str(ziomek[3])
+        print "O masz dziecko: " + dzieci[0]
+    # jeśli w ogóle nie ma dzieci, to powiedz, jak ma na nazwisko
     else:
-        print "Kto nie ma dzieci? " + str(ziomek[3])
+        print "Kto nie ma dzieci? " + ziomek[1]
 
 print '_' * 50
 
